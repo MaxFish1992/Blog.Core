8da4ac0d381da663b8f810b96f61618a40a7907c
MaxFish1992 <348756718@qq.com>
2018-11-22 15:08:19 +0800

    sqlSugarCore引入

---
+++ Blog.Core.Model/Models/Advertisement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Core.Model.Models
+{
+    /// <summary>
+    /// 功能描述    ：Advertisement  
+    /// 创 建 者    ：huangwei
+    /// 创建日期    ：2018/11/22 15:00:16 
+    /// 最后修改者  ：Administrator
+    /// 最后修改日期：2018/11/22 15:00:16 
+    /// </summary>
+    public class Advertisement
+    {
+    }
+}
--- Blog.Core.Repository/AdvertisementRepository.cs
+++ Blog.Core.Repository/AdvertisementRepository.cs
@@ -1,5 +1,8 @@
 ﻿using Blog.Core.IRepository;
+using Blog.Core.Model.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Blog.Core.Repository
 {
@@ -8,6 +11,26 @@ namespace Blog.Core.Repository
         public int Sum(int i, int j)
         {
             return i + j;
+        }
+
+        public int Add(Advertisement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Advertisement model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Advertisement model)
+        {
+            throw new NotImplementedException();
         }
     }
 }
