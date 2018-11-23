999e35b0ce7234ee5feab0ab0a35e03689e1f328
MaxFish1992 <348756718@qq.com>
2018-11-23 17:00:00 +0800

    BlogLogAOP

--- Blog.Core/AOP/BlogLogAOP.cs
+++ Blog.Core/AOP/BlogLogAOP.cs
@@ -14,7 +14,7 @@ namespace Blog.Core.AOP
         public void Intercept(IInvocation invocation)
         {
             //记录被拦截的方法信息
-            var dataIntercept = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}" + $"当前执行方法：{invocation.Method.Name}" + $"参数是：{string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray())} \r\n";
+            var dataIntercept = $"{DateTime.Now.ToString("yyyyMMddHHmmss")} \r\n" + $"当前执行方法：{invocation.Method.Name} \r\n" + $"参数是：{string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray())} \r\n";
 
             //在被拦截的方法执行完毕后，继续执行当前方法
             invocation.Proceed();
@@ -22,7 +22,8 @@ namespace Blog.Core.AOP
             dataIntercept += ($"被拦截方法执行完毕，返回结果：{invocation.ReturnValue}");
 
             #region 输出到当前项目日志
-            var path = Directory.GetCurrentDirectory() + @"\Log";
+            //var path = Directory.GetCurrentDirectory() + @"\Log";
+            var path = System.AppDomain.CurrentDomain.BaseDirectory + @"\Log";
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
